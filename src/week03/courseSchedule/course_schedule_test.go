d95b19f4871731ce6127eded477e4237d9cc5ed2
于鑫 <xin.yu@chaitin.com>
2021-07-06 14:58:02 +0800

    feat: handle course schedule II

---
+++ src/week03/courseSchedule/course_schedule_test.go
@@ -0,0 +1,46 @@
+package course_schedule_test
+
+import "testing"
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	var (
+		edges = make([][]int, numCourses)
+		visited = make([]int, numCourses)
+		valid = true
+		dfs func(u int)
+	)
+
+	dfs = func(u int) {
+		visited[u] = 1
+		for _, v := range edges[u] {
+			if visited[v] == 0 {
+				dfs(v)
+				if !valid {
+					return
+				}
+			} else if visited[v] == 1 {
+				valid = false
+				return
+			}
+		}
+		visited[u] = 2
+	}
+
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+	}
+
+	for i := 0; i < numCourses && valid; i++ {
+		if visited[i] == 0 {
+			dfs(i)
+		}
+	}
+	return valid
+}
+
+func TestCanFinish(t *testing.T) {
+	// t.Log(canFinish(2, [][]int{[]int{1, 0}}))
+	// t.Log(canFinish(2, [][]int{[]int{1, 0}, []int{0, 1}}))
+	// 5 [[1,4],[2,4],[3,1],[3,2]]
+	t.Log(canFinish(6, [][]int{[]int{1, 4}, []int{2, 4}, []int{4, 2}, []int{5, 4}, []int{3, 5}}))
+}
---
+++ src/week03/courseScheduleII/course_schedule_II_test.go
@@ -0,0 +1,57 @@
+package course_schedule_II_test
+
+import "testing"
+
+type Graph struct {
+	Edges [][]int
+	Visited []int
+	Result []int
+	Valid bool
+}
+
+func (g *Graph) dfs(u int) {
+	g.Visited[u] = 1
+	for _, v := range g.Edges[u] {
+		if g.Visited[v] == 0 {
+			g.dfs(v)
+			if !g.Valid {
+				return
+			}
+		} else if g.Visited[v] == 1 {
+			g.Valid = false
+			return
+		}
+	}
+	g.Visited[u] = 2
+	g.Result = append(g.Result, u)
+}
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	var g = Graph{}
+	g.Edges = make([][]int, numCourses)
+	g.Visited = make([]int, numCourses)
+	g.Valid = true
+
+	for _, info := range prerequisites {
+		g.Edges[info[1]] = append(g.Edges[info[1]], info[0])
+	}
+
+	for i := 0; i < numCourses && g.Valid; i++ {
+		if g.Visited[i] == 0 {
+			g.dfs(i)
+		}
+	}
+	if g.Valid {
+		l := len(g.Result)
+		for i := 0; i < l / 2; i++ {
+			g.Result[i], g.Result[l - i - 1] = g.Result[l - i - 1], g.Result[i]
+		}
+		return g.Result
+	} else {
+		return []int{}
+	}
+}
+
+func TestFindOrder(t *testing.T) {
+	t.Log(findOrder(2, [][]int{[]int{1, 0}}))
+}
