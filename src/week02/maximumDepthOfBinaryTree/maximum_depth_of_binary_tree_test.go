bc72e5c12df625209f86238c1760639bea4fe3cf
yuxin_cloud <zhuanyongxigua@gmail.com>
2021-10-07 08:30:55 +0800

    feat: add maximum_depth_of_binary_tree

---
+++ src/week02/maximumDepthOfBinaryTree/maximum_depth_of_binary_tree_test.go
@@ -0,0 +1,46 @@
+package maximum_depth_of_binary_tree_test
+
+import "testing"
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	var r func(curFloor int, root *TreeNode) int
+	r = func(curFloor int, root *TreeNode) int {
+		if root == nil {
+			return curFloor
+		}
+		curFloor += 1
+		leftMax := r(curFloor, root.Left)
+		rightMax := r(curFloor, root.Right)
+		if leftMax > rightMax {
+			return leftMax
+		} else {
+			return rightMax
+		}
+	}
+	ans := r(0, root)
+	return ans
+}
+
+func TestMaximumDepthOfBinaryTreeTest(t *testing.T) {
+	// tree := &TreeNode{
+	// 	120, 
+	// 	&TreeNode{
+	// 		70,
+	// 		&TreeNode{50, &TreeNode{20, nil, nil}, &TreeNode{55, nil, nil}},
+	// 		&TreeNode{100, &TreeNode{75, nil, nil}, &TreeNode{110, nil, nil}},
+	// 	}, 
+	// 	&TreeNode{
+	// 		140,
+	// 		&TreeNode{130, &TreeNode{119, nil, nil}, &TreeNode{135, nil, nil}},
+	// 		&TreeNode{160, &TreeNode{150, nil, nil}, &TreeNode{200, nil, nil}},
+	// 	}}
+	tree := &TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}
+	result := maxDepth(tree)
+	t.Log(result)
+}
