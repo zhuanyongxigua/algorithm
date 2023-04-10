144d2a1acb071d512ed3a9500c6d948c7284d8fa
于鑫 <yuxin@yuxins-MBP.lan>
2023-04-10 22:55:10 +0800

    feat: handle dfs of course_schedule_II_test.go

--- src/week03/courseScheduleII/course_schedule_II_test.go
+++ src/week03/courseScheduleII/course_schedule_II_test.go
@@ -1,8 +1,61 @@
 package course_schedule_II_test
 
 import (
+	"fmt"
 	"testing"
 )
+
+// 这一坨 dfs 虽然不太规整，但是思路还是有点牛逼的，很久以前的我写的，居然还可以倒着收集
+type Graph struct {
+	Edges   [][]int
+	Visited []int
+	Result  []int
+	Valid   bool
+}
+
+func (g *Graph) dfs(u int) {
+	g.Visited[u] = 1
+	for _, v := range g.Edges[u] {
+		if g.Visited[v] == 0 {
+			g.dfs(v)
+			if !g.Valid {
+				return
+			}
+		} else if g.Visited[v] == 1 {
+			g.Valid = false
+			return
+		}
+	}
+	g.Visited[u] = 2
+	g.Result = append(g.Result, u)
+}
+
+func findOrder3(numCourses int, prerequisites [][]int) []int {
+	var g = Graph{}
+	g.Edges = make([][]int, numCourses)
+	g.Visited = make([]int, numCourses)
+	g.Valid = true
+
+	for _, info := range prerequisites {
+		g.Edges[info[1]] = append(g.Edges[info[1]], info[0])
+	}
+
+	for i := 0; i < numCourses && g.Valid; i++ {
+		if g.Visited[i] == 0 {
+			g.dfs(i)
+		}
+	}
+	fmt.Printf("Result after dfs: %v\n", g.Result)
+	if g.Valid {
+		l := len(g.Result)
+		for i := 0; i < l/2; i++ {
+			g.Result[i], g.Result[l-i-1] = g.Result[l-i-1], g.Result[i]
+		}
+		return g.Result
+	} else {
+		return []int{}
+	}
+}
 
 type OutLineItem struct {
 	Lines []int
@@ -25,32 +78,30 @@ func findOrder(numCourses int, prerequisites [][]int) []int {
 		}
 		edges[i].Visited = true
 		for _, v := range edges[i].Lines {
-			edges[v].InDegree--
-			if edges[v].Marked == false && edges[v].InDegree == 0 {
-				edges[v].Marked = true
-				ans = append(ans, v)
-			}
 			valid := dfs(v)
 			if valid == false {
 				return false
 			}
 			edges[v].Visited = false
 		}
+		if edges[i].Marked == false {
+			ans = append(ans, i)
+			edges[i].Marked = true
+		}
 		return true
 	}
 	for i := range edges {
 		valid := true
-		if edges[i].InDegree == 0 {
-			if edges[i].Marked == false {
-				edges[i].Marked = true
-				ans = append(ans, i)
-			}
-			valid = dfs(i)
-			edges[i].Visited = false
-		}
+		valid = dfs(i)
+		edges[i].Visited = false
 		if valid == false {
 			return []int{}
 		}
+	}
+
+	l := len(ans)
+	for i := 0; i < l/2; i++ {
+		ans[i], ans[l-i-1] = ans[l-i-1], ans[i]
 	}
 	return ans
 }
@@ -97,5 +148,6 @@ func TestFindOrder(t *testing.T) {
 	// t.Log(findOrder(2, [][]int{}))
 	// t.Log(findOrder(2, [][]int{[]int{0, 1}, []int{1, 0}}))
 	// t.Log(findOrder(3, [][]int{[]int{1, 0}, []int{1, 2}, []int{0, 1}}))
-	t.Log(findOrder(7, [][]int{[]int{1, 0}, []int{0, 3}, []int{0, 2}, []int{3, 2}, []int{2, 5}, []int{4, 5}, []int{5, 6}, []int{2, 4}}))
+	// t.Log(findOrder(7, [][]int{[]int{1, 0}, []int{0, 3}, []int{0, 2}, []int{3, 2}, []int{2, 5}, []int{4, 5}, []int{5, 6}, []int{2, 4}}))
+	t.Log(findOrder(8, [][]int{[]int{1, 0}, []int{2, 6}, []int{1, 7}, []int{5, 1}, []int{6, 4}, []int{7, 0}, []int{0, 5}}))
 }
